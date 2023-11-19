fc29cea30e9e90b351270105516fe52cedb03605
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-19 13:51:15 +0700

    chore: add more fields to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject learn-clojure "0.1.0-SNAPSHOT"
-  :description "A Clojure project"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main learn-clojure.core)
+  :description "I am learning Clojure"
+  :url "http://github.com/duong755/learn-clojure"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot learn-clojure.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
