b18359225f2cb6e802f47ec0b19b2f54b9832e6d
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-24 22:04:47 +0700

    feat(4ever-clojure): 3 more solutions to problem 20

--- src/learn_clojure/problem_20.clj
+++ src/learn_clojure/problem_20.clj
@@ -7,4 +7,27 @@
           (nth x (- (count x) 2)))]
     (assert (= (result (list 1 2 3 4 5)) 4))
     (assert (= (result ["a" "b" "c"]) "b"))
-    (assert (= (result [[1 2] [3 4]]) [1 2]))))
+    (assert (= (result [[1 2] [3 4]]) [1 2])))
+  (let [result-2
+        (fn second-to-last [x]
+          (last (butlast x)))]
+    (assert (= (result-2 (list 1 2 3 4 5)) 4))
+    (assert (= (result-2 ["a" "b" "c"]) "b"))
+    (assert (= (result-2 [[1 2] [3 4]]) [1 2])))
+  (let [result-3
+        (fn second-to-last [x]
+          (if (= (count x) 2)
+            (first x)
+            (second-to-last (rest x))))]
+    (assert (= (result-3 (list 1 2 3 4 5)) 4))
+    (assert (= (result-3 ["a" "b" "c"]) "b"))
+    (assert (= (result-3 [[1 2] [3 4]]) [1 2])))
+  (let [result-4
+        (fn second-to-last [x]
+          (loop [input-list x]
+            (if (= (count input-list) 2)
+              (first input-list)
+              (recur (rest input-list)))))]
+    (assert (= (result-4 (list 1 2 3 4 5)) 4))
+    (assert (= (result-4 ["a" "b" "c"]) "b"))
+    (assert (= (result-4 [[1 2] [3 4]]) [1 2]))))
