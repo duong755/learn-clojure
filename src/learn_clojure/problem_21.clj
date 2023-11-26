ea41479587c00dd2468085bcf8fe4c192f5fa2a2
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-26 13:25:34 +0700

    feat(4ever-clojure): 1 more solution to problem 21

--- src/learn_clojure/problem_21.clj
+++ src/learn_clojure/problem_21.clj
@@ -4,11 +4,22 @@
   (println "Problem 21, Nth Element")
   (let [result
         (fn element-at [x y]
-          (if (= y 0)
+          (if (zero? y)
             (first x)
             (element-at
              (rest x) (dec y))))]
     (assert (= (result '(4 5 6 7) 2) 6))
     (assert (= (result [:a :b :c] 0) :a))
     (assert (= (result [1 2 3 4] 1) 2))
-    (assert (= (result '([1 2] [3 4] [5 6]) 2) [5 6]))))
+    (assert (= (result '([1 2] [3 4] [5 6]) 2) [5 6])))
+  (let [result-2
+        (fn element-at [x y]
+          (loop [my-list x
+                 index y]
+            (if (zero? index)
+              (first my-list)
+              (recur (rest my-list) (dec index)))))]
+    (assert (= (result-2 '(4 5 6 7) 2) 6))
+    (assert (= (result-2 [:a :b :c] 0) :a))
+    (assert (= (result-2 [1 2 3 4] 1) 2))
+    (assert (= (result-2 '([1 2] [3 4] [5 6]) 2) [5 6]))))
