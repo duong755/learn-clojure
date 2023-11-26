ac89e313370b9e6f0da2216d9ebf1d6d63de4eb0
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-26 14:10:06 +0700

    feat(4ever-clojure): 1 more solution to problem 27

--- src/learn_clojure/problem_27.clj
+++ src/learn_clojure/problem_27.clj
@@ -11,4 +11,16 @@
     (assert (true? (result "racecar")))
     (assert (true? (result [:foo :bar :foo])))
     (assert (true? (result '(1 1 3 3 1 1))))
-    (assert (false? (result '(:a :b :c))))))
+    (assert (false? (result '(:a :b :c)))))
+  (let [result-2
+        (fn [s]
+          (loop [input-seq s]
+            (cond
+              (< (count input-seq) 2) true
+              (not= (first input-seq) (last input-seq)) false
+              :else (recur (rest (butlast input-seq))))))]
+    (assert (false? (result-2 '(1 2 3 4 5))))
+    (assert (true? (result-2 "racecar")))
+    (assert (true? (result-2 [:foo :bar :foo])))
+    (assert (true? (result-2 '(1 1 3 3 1 1))))
+    (assert (false? (result-2 '(:a :b :c))))))
