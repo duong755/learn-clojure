25946e51450873d481b96f1ddb443dd4b5b14304
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-26 13:02:40 +0700

    feat(4ever-clojure): 1 more solution to problem 29

--- src/learn_clojure/problem_29.clj
+++ src/learn_clojure/problem_29.clj
@@ -1,4 +1,5 @@
 (ns learn-clojure.problem-29
+  (:import java.lang.Character)
   (:require [clojure.string :as str]))
 
 (defn problem_29 []
@@ -11,4 +12,18 @@
             (reduce str (filter is-upper-letter x))))]
     (assert (= (result "HeLlO, WoRlD!") "HLOWRD"))
     (assert (empty? (result "nothing")))
-    (assert (= (result "$#A(*&987Zf") "AZ"))))
+    (assert (= (result "$#A(*&987Zf") "AZ")))
+  ;; the following solution does not work in 4ever-clojure
+  (let [result-2
+        (fn [x]
+          (let [char-code-A (Character/codePointAt "A" 0)
+                char-code-Z (Character/codePointAt "Z" 0)
+                is-upper-letter (fn [c]
+                                  (let [char-code-input (Character/codePointAt (str c) 0)]
+                                    (and
+                                     (>= char-code-input char-code-A)
+                                     (<= char-code-input char-code-Z))))]
+             (reduce str (filter is-upper-letter x))))]
+    (assert (= (result-2 "HeLlO, WoRlD!") "HLOWRD"))
+    (assert (empty? (result-2 "nothing")))
+    (assert (= (result-2 "$#A(*&987Zf") "AZ"))))
