3936f79eb0f3b4d92992744b416238f45e68a0de
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-26 13:13:05 +0700

    feat(4ever-clojure): 1 more solution to problem 26

--- src/learn_clojure/problem_26.clj
+++ src/learn_clojure/problem_26.clj
@@ -13,4 +13,18 @@
                           (conj nil (+ (last previous) (nth previous (- (count previous) 2))))))))))]
     (assert (= (result 3) '(1 1 2)))
     (assert (= (result 6) '(1 1 2 3 5 8)))
-    (assert (= (result 8) '(1 1 2 3 5 8 13 21)))))
+    (assert (= (result 8) '(1 1 2 3 5 8 13 21))))
+  (let [result-2
+        (fn fib [n]
+          (cond
+            (= n 0) '()
+            (= n 1) '(1)
+            (= n 2) '(1 1)
+            :else (let [previous (fib (dec n))]
+                    (concat previous
+                            (conj nil
+                                  (+ (last previous)
+                                     (last (butlast previous))))))))]
+    (assert (= (result-2 3) '(1 1 2)))
+    (assert (= (result-2 6) '(1 1 2 3 5 8)))
+    (assert (= (result-2 8) '(1 1 2 3 5 8 13 21)))))
