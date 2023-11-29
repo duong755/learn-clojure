0cf66585ac2417221d4a060678352452a02c3f5d
duong755 <ngo.quang.duong.1100@gmail.com>
2023-11-29 23:04:45 +0700

    feat(4ever-clojure): 1 more solution to problem 22, and 1 more solution to problem 23

--- src/learn_clojure/problem_22.clj
+++ src/learn_clojure/problem_22.clj
@@ -4,9 +4,21 @@
   (println "Problem 22, Count a Sequence")
   (let [result
         (fn my-count [x]
-          (if (nil? (first x))
+          (if (empty? x)
             0
             (inc (my-count (rest x)))))]
+    (assert (= (result '(1 2 3 3 1)) 5))
+    (assert (= (result "Hello World") 11))
+    (assert (= (result [[1 2] [3 4] [5 6]]) 3))
+    (assert (= (result '(13)) 1))
+    (assert (= (result '(:a :b :c)) 3)))
+  (let [result
+        (fn my-count [x]
+          (loop [length 0
+                 input-list x]
+            (if (empty? input-list)
+              length
+              (recur (inc length) (rest input-list)))))]
     (assert (= (result '(1 2 3 3 1)) 5))
     (assert (= (result "Hello World") 11))
     (assert (= (result [[1 2] [3 4] [5 6]]) 3))
--- src/learn_clojure/problem_23.clj
+++ src/learn_clojure/problem_23.clj
@@ -14,4 +14,19 @@
     (assert
      (= (result (sorted-set 5 7 2 7)) '(7 5 2)))
     (assert
+     (= (result [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]])))
+  (let [result
+        (fn my-reverse [x]
+            (loop [rest-of-x (rest x)
+                   first-of-x (list (first x))]
+              (if (<= (count rest-of-x) 1)
+                (concat rest-of-x first-of-x)
+                (recur
+                 (rest rest-of-x)
+                 (concat (list (first rest-of-x)) first-of-x)))))]
+    (assert
+     (= (result [1 2 3 4 5]) [5 4 3 2 1]))
+    (assert
+     (= (result (sorted-set 5 7 2 7)) '(7 5 2)))
+    (assert
      (= (result [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]]))))
