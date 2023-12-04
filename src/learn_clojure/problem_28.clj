368e959ef5d822fa72f8e90aa507d2d615dd574d
duong755 <ngo.quang.duong.1100@gmail.com>
2023-12-04 20:41:09 +0700

    feat(4ever-clojure): 1 more solution to problem 28, 2 more solutions to problem 39

--- src/learn_clojure/problem_28.clj
+++ src/learn_clojure/problem_28.clj
@@ -9,9 +9,28 @@
                (result-1-flatten y)
                (conj nil y)))) (empty s) s))
 
+(defn result-2-flatten
+  [s]
+  (loop [result (empty s)
+         my-seq s]
+    (if (empty? my-seq)
+      result
+      (let [[x & xs] my-seq]
+        (if (empty? xs)
+          (if (coll? x)
+            (recur result x)
+            (concat result (conj nil x)))
+          (if (coll? x)
+            (recur result (concat x xs))
+            (recur (concat result (conj nil x)) xs)))))))
+
 (defn problem_28 []
   (println "Problem 28, Flatten a Sequence")
 
   (assert (= (result-1-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
   (assert (= (result-1-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
-  (assert (= (result-1-flatten '((((:a))))) '(:a))))
+  (assert (= (result-1-flatten '((((:a))))) '(:a)))
+
+  (assert (= (result-2-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
+  (assert (= (result-2-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
+  (assert (= (result-2-flatten '((((:a))))) '(:a))))
--- src/learn_clojure/problem_39.clj
+++ src/learn_clojure/problem_39.clj
@@ -26,9 +26,45 @@
            rest-right
            (concat result (conj nil first-right))))))))
 
+(defn result-2-interleave
+  [s1 s2]
+  (let [min-len (min (count s1) (count s2))]
+    (loop [n 0
+           result '()]
+      (if (>= n min-len)
+        result
+        (recur (inc n) (concat result (conj nil (nth s2 n) (nth s1 n))))))))
+
+(defn result-3-interleave
+  [s1 s2]
+  (let [min-len (min (count s1) (count s2))]
+    (loop [n 0
+           left s1
+           right s2
+           result '()]
+      (if (>= n min-len)
+        result
+        (recur
+         (inc n)
+         (rest left)
+         (rest right)
+         (concat result (conj nil (first right) (first left))))))))
+
+
 (defn problem_39 []
   (println "Problem 39, Interleave Two Seqs")
+
   (assert (= (result-1-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))
   (assert (= (result-1-interleave [1 2] [3 4 5 6]) '(1 3 2 4)))
   (assert (= (result-1-interleave [1 2 3 4] [5]) [1 5]))
-  (assert (= (result-1-interleave [30 20] [25 15]) [30 25 20 15])))
+  (assert (= (result-1-interleave [30 20] [25 15]) [30 25 20 15]))
+
+  (assert (= (result-2-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))
+  (assert (= (result-2-interleave [1 2] [3 4 5 6]) '(1 3 2 4)))
+  (assert (= (result-2-interleave [1 2 3 4] [5]) [1 5]))
+  (assert (= (result-2-interleave [30 20] [25 15]) [30 25 20 15]))
+
+  (assert (= (result-3-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))
+  (assert (= (result-3-interleave [1 2] [3 4 5 6]) '(1 3 2 4)))
+  (assert (= (result-3-interleave [1 2 3 4] [5]) [1 5]))
+  (assert (= (result-3-interleave [30 20] [25 15]) [30 25 20 15])))
