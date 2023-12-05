8c908e1d3b052612170ec6aff38629d9a86297a3
duong755 <ngo.quang.duong.1100@gmail.com>
2023-12-05 22:17:12 +0700

    feat(4ever-clojure): 1 more solution to problem 41, 1 more solution to problem 43

--- src/learn_clojure/problem_41.clj
+++ src/learn_clojure/problem_41.clj
@@ -16,6 +16,17 @@
        (concat result (take (dec n) the-rest))
        (drop n the-rest)))))
 
+(defn result-3-drop-every-nth
+  [s n]
+  (loop [result (empty s)
+         index 0
+         the-rest s]
+    (if (>= index (count s))
+      result
+      (if (zero? (rem (inc index) n))
+        (recur result (inc index) (rest the-rest))
+        (recur (concat result (conj nil (first the-rest))) (inc index) (rest the-rest))))))
+
 (defn problem_41 []
   (println "Problem 41, Drop Every Nth Item")
 
@@ -25,4 +36,8 @@
 
   (assert (= (result-2-drop-every-nth [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
   (assert (= (result-2-drop-every-nth [:a :b :c :d :e :f] 2) [:a :c :e]))
-  (assert (= (result-2-drop-every-nth [1 2 3 4 5 6] 4) [1 2 3 5 6])))
+  (assert (= (result-2-drop-every-nth [1 2 3 4 5 6] 4) [1 2 3 5 6]))
+
+  (assert (= (result-3-drop-every-nth [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8]))
+  (assert (= (result-3-drop-every-nth [:a :b :c :d :e :f] 2) [:a :c :e]))
+  (assert (= (result-3-drop-every-nth [1 2 3 4 5 6] 4) [1 2 3 5 6])))
--- src/learn_clojure/problem_43.clj
+++ src/learn_clojure/problem_43.clj
@@ -53,8 +53,43 @@
         (recur
          (concat result (list (take-every the-rest next-n))) (drop-every the-rest next-n) (dec next-n))))))
 
+(defn result-2-reverse-interleave
+  [s n]
+  (let [take-and-drop (fn [input-seq number]
+                        (if (empty? input-seq)
+                          input-seq
+                          (loop [take-result (empty input-seq),
+                                 drop-result (empty input-seq),
+                                 the-rest input-seq, ;; first and rest are preferrable because nth is O(n)
+                                 index 0]
+                            (if (>= index (count input-seq))
+                              [take-result drop-result]
+                              (if (zero? (rem index number))
+                                (recur
+                                 (concat take-result (conj nil (first the-rest)))
+                                 drop-result
+                                 (rest the-rest)
+                                 (inc index))
+                                (recur
+                                 take-result
+                                 (concat drop-result (conj nil (first the-rest)))
+                                 (rest the-rest)
+                                 (inc index)))))))]
+    (loop [result '()
+           the-rest s
+           next-n n]
+      (if (empty? the-rest)
+        result
+        (let [[take-result drop-result] (take-and-drop the-rest next-n)]
+          (recur (concat result (list take-result)) drop-result (dec next-n)))))))
+
+
 (defn problem_43 []
   (println "Problem 43, Reverse Interleave")
   (assert (= (result-1-reverse-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6))))
   (assert (= (result-1-reverse-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8))))
-  (assert (= (result-1-reverse-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))))
+  (assert (= (result-1-reverse-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9))))
+
+  (assert (= (result-2-reverse-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6))))
+  (assert (= (result-2-reverse-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8))))
+  (assert (= (result-2-reverse-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))))
