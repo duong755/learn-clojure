ade63a233ff25372245470dfe01a37f02a5e3737
duong755 <ngo.quang.duong.1100@gmail.com>
2023-12-15 22:31:18 +0700

    feat(4ever-clojure): 1 more solution to problem 66, 1 more solution to problem 69

--- src/learn_clojure/problem_66.clj
+++ src/learn_clojure/problem_66.clj
@@ -6,9 +6,22 @@
     b
     (result-1-gcd b (rem a b))))
 
+(defn result-2-gcd
+  [a b]
+  (loop [x a
+         y b]
+    (if (zero? (rem x y))
+      y
+      (recur y (rem x y)))))
+
 (defn problem_66 []
   (println "Problem 66, Greatest Common Divisor")
   (assert (= (result-1-gcd 2 4) 2))
   (assert (= (result-1-gcd 10 5) 5))
   (assert (= (result-1-gcd 5 7) 1))
-  (assert (= (result-1-gcd 1023 858) 33)))
+  (assert (= (result-1-gcd 1023 858) 33))
+
+  (assert (= (result-2-gcd 2 4) 2))
+  (assert (= (result-2-gcd 10 5) 5))
+  (assert (= (result-2-gcd 5 7) 1))
+  (assert (= (result-2-gcd 1023 858) 33)))
--- src/learn_clojure/problem_69.clj
+++ src/learn_clojure/problem_69.clj
@@ -16,6 +16,20 @@
        (first maps)
        (rest maps))))
 
+(defn result-2-merge-with
+  [f & maps]
+  (if (empty? maps)
+    {}
+    (reduce
+     (fn [merge-map current-map]
+       (reduce-kv
+        (fn [m key val]
+          (if (contains? m key)
+            (update m key f val)
+            (assoc m key val))) merge-map current-map))
+     {}
+     maps)))
+
 (defn problem_69 []
   (println "Problem 69, Merge with a Function")
   (assert (= (result-1-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
@@ -23,4 +37,11 @@
   (assert (= (result-1-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15})
              {1 7, 2 10, 3 15}))
   (assert (= (result-1-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+             {:a [3 4 5], :b [6 7], :c [8 9]}))
+
+  (assert (= (result-2-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+             {:a 4, :b 6, :c 20}))
+  (assert (= (result-2-merge-with - {1 10, 2 20} {1 3, 2 10, 3 15})
+             {1 7, 2 10, 3 15}))
+  (assert (= (result-2-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
              {:a [3 4 5], :b [6 7], :c [8 9]})))
