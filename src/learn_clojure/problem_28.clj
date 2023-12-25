32107857dcaaddbbc8581f02ee94b9e68192e666
duong755 <ngo.quang.duong.1100@gmail.com>
2023-12-25 18:06:09 +0700

    feat(4ever-clojure): 1 more solution to problem 28

--- src/learn_clojure/problem_28.clj
+++ src/learn_clojure/problem_28.clj
@@ -24,6 +24,18 @@
             (recur result (concat x xs))
             (recur (concat result (conj nil x)) xs)))))))
 
+(defn result-3-flatten
+  [s]
+  (loop [result (empty s)
+         my-seq s]
+    (if (empty? my-seq)
+      result
+      (let [[x & xs] my-seq]
+        (if (coll? x)
+          (recur result (concat x xs))
+          (recur (concat result (conj nil x)) xs))))))
+
+
 (defn problem_28 []
   (println "Problem 28, Flatten a Sequence")
 
@@ -33,4 +45,8 @@
 
   (assert (= (result-2-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
   (assert (= (result-2-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
-  (assert (= (result-2-flatten '((((:a))))) '(:a))))
+  (assert (= (result-2-flatten '((((:a))))) '(:a)))
+
+  (assert (= (result-3-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))
+  (assert (= (result-3-flatten ["a" ["b"] "c"]) '("a" "b" "c")))
+  (assert (= (result-3-flatten '((((:a))))) '(:a))))
