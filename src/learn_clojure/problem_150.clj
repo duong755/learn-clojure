32dca9fd28bc281991b428f067b82bf6e829a758
duong755 <ngo.quang.duong.1100@gmail.com>
2024-02-09 00:32:52 +0700

    feat(4ever-clojure): 1 more solution to problem 150

--- src/learn_clojure/problem_150.clj
+++ src/learn_clojure/problem_150.clj
@@ -55,6 +55,54 @@
   [n]
   (iterate next-palindrome (closest-palindrome n)))
 
+(defn palindrome-2
+  [n]
+  (let [int->digits
+        (fn [n]
+          (loop [result []
+                 quotient n]
+            (if (zero? quotient)
+              (if (empty? result) [0] result)
+              (recur (cons (rem quotient 10) result) (quot quotient 10)))))
+        digits->int
+        (fn [digits]
+          (loop [result 0
+                 power 1
+                 digits digits]
+            (if (empty? digits)
+              result
+              (recur (+ result (* power (last digits))) (* power 10) (butlast digits)))))
+        next-palindrome
+        (fn [n]
+          (let [length (count (str n))
+                n-digits (int->digits n)
+                half (take (Math/ceil (/ length 2)) n-digits)
+                new-half (int->digits (inc (digits->int half)))
+                the-head (if (even? length)
+                           new-half
+                           (if (= (count half) (count new-half))
+                             new-half
+                             (butlast new-half)))
+                the-rest (if (even? length)
+                           (if (= (count half) (count new-half))
+                             (reverse new-half)
+                             (reverse (butlast new-half)))
+                           (reverse (butlast new-half)))]
+            (digits->int (concat the-head the-rest))))
+        closest-palindrome
+        (fn [n]
+          (let [length (count (str n))
+                n-digits (int->digits n)
+                the-head (take (Math/ceil (/ length 2)) n-digits)
+                the-rest (if (even? length)
+                           (reverse the-head)
+                           (reverse (butlast the-head)))
+                smallest (digits->int (concat the-head the-rest))]
+            (if (>= smallest n)
+              smallest
+              (closest-palindrome (next-palindrome smallest)))))]
+    (iterate next-palindrome (closest-palindrome n))))
+
 (defn problem_150
   []
   (println "Problem 150, Palindromic Numbers")
@@ -73,6 +121,23 @@
   (assert (= (first (palindrome (* 111111111 111111111)))
              (* 111111111 111111111)))
   (assert (= (set (take 199 (palindrome 0)))
-             (set (map #(first (palindrome %)) (range 0 10000))))))
+             (set (map #(first (palindrome %)) (range 0 10000)))))
+
+  (assert (= (take 26 (palindrome-2 0))
+             [0 1 2 3 4 5 6 7 8 9
+              11 22 33 44 55 66 77 88 99
+              101 111 121 131 141 151 161]))
+  (assert (= (take 16 (palindrome-2 162))
+             [171 181 191 202
+              212 222 232 242
+              252 262 272 282
+              292 303 313 323]))
+  (assert (= (take 6 (palindrome-2 1234550000))
+             [1234554321 1234664321 1234774321
+              1234884321 1234994321 1235005321]))
+  (assert (= (first (palindrome-2 (* 111111111 111111111)))
+             (* 111111111 111111111)))
+  (assert (= (set (take 199 (palindrome-2 0)))
+             (set (map #(first (palindrome-2 %)) (range 0 10000))))))
 
 
